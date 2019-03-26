976e9e4e1020b3987dca15af92620309bacd82fc
Dru Kelly <drukelly@gmail.com>
2019-03-26 14:27:57 -0700

    selecting character

--- assets/js/game.js
+++ assets/js/game.js
@@ -37,20 +37,35 @@ $(function(){
   const attack = (player, enemy) => {
 
   }
+  const displayStats = (selector, player) => {
+
+  }
   const selectCharacter = (selector, player) => {
-   
+    let template = `<img src="assets/images/${player.replace(" ", "-").toLowerCase()}.jpg" alt="${player}" class="border border-light">`; 
+    $(selector).find("figure").html(template);
+  }
+  const superPower = (player, enemy) => {
+
   }
 
-
+  // render heroes list items
   $.each(heroes, (index, value) => {
-    let template = `<li><a href="#" data-character="${value}">${value}</a></li>`
+    let template = `<li class="mr-3"><a href="#" class="character" title="${value}" data-character="${value}" data-character-type="hero"><img src="assets/images/${value.replace(" ", "-").toLowerCase()}.jpg" alt="${value}" class="border border-secondary"></a></li>`
     $("#heroes").append(template);
   });
-  
+  // render villain list items
   $.each(villains, (index, value) => {
-    let template = `<li><a href="#" data-character="${value}">${value}</a></li>`
+    let template = `<li class="mr-3"><a href="#" class="character" title="${value}" data-character="${value}" data-character-type="villain"><img src="assets/images/${value.replace(" ", "-").toLowerCase()}.jpg" alt="${value}" class="border border-secondary"></a></li>`
     $("#villains").append(template);
   });
-
-  
+  // onclick
+  $(".character").on("click", (event) => {
+    event.preventDefault();
+    if (event.currentTarget.dataset.characterType === "hero") {
+      selectCharacter(".hero", event.currentTarget.dataset.character);
+    }
+    if (event.currentTarget.dataset.characterType === "villain") {
+      selectCharacter(".villain", event.currentTarget.dataset.character);
+    }
+  });
 });
