627a21dd74eae84fdac39779908e6a8a2a8e4060
Dru Kelly <drukelly@gmail.com>
2019-03-26 15:17:29 -0700

    refactor

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,31 +1,26 @@
 $(function(){
-  let heroes = ["Han Solo", "Luke Skywalker"];
-  let villains = ["Boba Fett", "Darth Vader"];
+  let heroes = ["Han Solo", "Luke Skywalker"],
+      villains = ["Boba Fett", "Darth Vader"];
+
+  let selectedHero,
+      selectedVillain;
 
   let BobaFett = {
-    firstName:  "Boba",
-    lastName:   "Fett",
     class:      "Villain",
     hp:         150,
     attackMax:  25
   }
   let DarthVader = {
-    firstName:  "Darth",
-    lastname:   "Vader",
     class:      "Villain",
     hp:         200,
     attackMax:  30
   }
   let HanSolo = {
-    firstName:  "Han",
-    lastName:   "Solo",
     class:      "Hero",
     hp:         125,
     attackMax:  20
   }
   let LukeSkywalker = {
-    firstName:  "Luke",
-    lastName:   "Skywalker",
     class:      "Hero",
     hp:         100,
     attackMax:  20
@@ -44,9 +39,6 @@ $(function(){
     let template = `<img src="assets/images/${player.replace(" ", "-").toLowerCase()}.jpg" alt="${player}" class="border border-light">`; 
     $(selector).find("figure").html(template);
   }
-  const superPower = (player, enemy) => {
-
-  }
 
   // render heroes list items
   $.each(heroes, (index, value) => {
@@ -58,14 +50,31 @@ $(function(){
     let template = `<li class="mr-3"><a href="#" class="character" title="${value}" data-character="${value}" data-character-type="villain"><img src="assets/images/${value.replace(" ", "-").toLowerCase()}.jpg" alt="${value}" class="border border-secondary"></a></li>`
     $("#villains").append(template);
   });
-  // onclick
+  // character select
   $(".character").on("click", (event) => {
     event.preventDefault();
     if (event.currentTarget.dataset.characterType === "hero") {
       selectCharacter(".hero", event.currentTarget.dataset.character);
+      $(".controls").removeClass("hide");
+      selectedHero = event.currentTarget.dataset.character;
     }
     if (event.currentTarget.dataset.characterType === "villain") {
       selectCharacter(".villain", event.currentTarget.dataset.character);
+      selectedVillain = event.currentTarget.dataset.character;
     }
+  });
+  // attack!
+  $("#Attack").on("click", (event) => {
+    event.preventDefault();
+    if (selectedVillain == null) {
+      alert("Select a villain first");
+    } else {
+      console.log("attack!");
+    }
+  });
+  // defend
+  $("#Shield").on("click", (event) => {
+    event.preventDefault();
+    console.log("shielded!");
   });
 });
