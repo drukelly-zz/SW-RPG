2878c8e08828bab09e718f1674ec3faa9a6c2f45
Dru Kelly <drukelly@gmail.com>
2019-03-27 00:37:42 -0700

    display hp

--- assets/js/game.js
+++ assets/js/game.js
@@ -2,38 +2,72 @@ $(function(){
   let heroes = ["Han Solo", "Luke Skywalker"],
       villains = ["Boba Fett", "Darth Vader"];
 
-  let selectedHero,
+  let attackPower,
+      selectedHero,
       selectedVillain;
 
   let BobaFett = {
     class:      "Villain",
     hp:         150,
-    attackMax:  25
+    attackMax:  25,
+    getHP: function() {
+      return this.hp;
+    },
+    getAttack: function() {
+      return Math.floor(Math.random() * (this.attackMax - 1)) + 1;
+    }
   }
   let DarthVader = {
     class:      "Villain",
     hp:         200,
-    attackMax:  30
+    attackMax:  30,
+    getHP: function() {
+      return this.hp;
+    },
+    getAttack: function() {
+      return Math.floor(Math.random() * (this.attackMax - 1)) + 1;
+    }
   }
   let HanSolo = {
     class:      "Hero",
-    hp:         125,
-    attackMax:  20
+    hp:         90,
+    attackMax:  20,
+    getHP: function() {
+      return this.hp;
+    },
+    getAttack: function() {
+      return Math.floor(Math.random() * (this.attackMax - 1)) + 1;
+    }
   }
   let LukeSkywalker = {
     class:      "Hero",
-    hp:         100,
-    attackMax:  20
-  }
-
-  const activateShield = (player) => {
-
+    hp:         125,
+    attackMax:  20,
+    getHP: function() {
+      return this.hp;
+    },
+    getAttack: function() {
+      return Math.floor(Math.random() * (this.attackMax - 1)) + 1;
+    }
   }
   const attack = (player, enemy) => {
 
   }
   const displayStats = (selector, player) => {
-
+    switch (player) {
+      case "Boba Fett":
+        $(selector).find('.stats').text(BobaFett.getHP());
+        break;
+      case "Darth Vader":
+        $(selector).find('.stats').text(DarthVader.getHP());
+        break;
+      case "Han Solo":
+        $(selector).find('.stats').text(HanSolo.getHP());
+        break;
+      case "Luke Skywalker":
+        $(selector).find('.stats').text(LukeSkywalker.getHP());
+        break;
+    }
   }
   const selectCharacter = (selector, player) => {
     let template = `<img src="assets/images/${player.replace(" ", "-").toLowerCase()}.jpg" alt="${player}" class="border border-light">`; 
@@ -42,12 +76,22 @@ $(function(){
 
   // render heroes list items
   $.each(heroes, (index, value) => {
-    let template = `<li class="mr-3"><a href="#" class="character" title="${value}" data-character="${value}" data-character-type="hero"><img src="assets/images/${value.replace(" ", "-").toLowerCase()}.jpg" alt="${value}" class="border border-secondary"></a></li>`
+    let template =
+      `<li class="mr-3">
+        <a href="#" class="character" title="${value}" data-character="${value}" data-character-type="hero">
+          <img src="assets/images/${value.replace(" ", "-").toLowerCase()}.jpg" alt="${value}" class="border border-secondary">
+        </a>
+      </li>`;
     $("#heroes").append(template);
   });
   // render villain list items
   $.each(villains, (index, value) => {
-    let template = `<li class="mr-3"><a href="#" class="character" title="${value}" data-character="${value}" data-character-type="villain"><img src="assets/images/${value.replace(" ", "-").toLowerCase()}.jpg" alt="${value}" class="border border-secondary"></a></li>`
+    let template = 
+      `<li class="mr-3">
+        <a href="#" class="character" title="${value}" data-character="${value}" data-character-type="villain">
+          <img src="assets/images/${value.replace(" ", "-").toLowerCase()}.jpg" alt="${value}" class="border border-secondary">
+        </a>
+      </li>`;
     $("#villains").append(template);
   });
   // character select
@@ -57,24 +101,21 @@ $(function(){
       selectCharacter(".hero", event.currentTarget.dataset.character);
       $(".controls").removeClass("hide");
       selectedHero = event.currentTarget.dataset.character;
+      displayStats(".hero", selectedHero);
     }
     if (event.currentTarget.dataset.characterType === "villain") {
       selectCharacter(".villain", event.currentTarget.dataset.character);
       selectedVillain = event.currentTarget.dataset.character;
+      displayStats(".villain", selectedVillain);
     }
   });
   // attack!
-  $("#Attack").on("click", (event) => {
+  $("#Attack").on("click", (event, attackPower) => {
     event.preventDefault();
     if (selectedVillain == null) {
       alert("Select a villain first");
     } else {
-      console.log("attack!");
+      console.log("attack! " + attackPower);
     }
-  });
-  // defend
-  $("#Shield").on("click", (event) => {
-    event.preventDefault();
-    console.log("shielded!");
   });
 });
